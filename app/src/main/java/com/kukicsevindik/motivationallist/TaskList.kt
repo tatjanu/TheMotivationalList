cdc71daf4eceb57cd09c882b9d26a93205012e81
Tatjana <tatjana-kukic@hotmail.de>
2019-11-19 14:53:47 +0100

    Changed the ?

--- app/src/main/java/com/kukicsevindik/motivationallist/TaskList.kt
+++ app/src/main/java/com/kukicsevindik/motivationallist/TaskList.kt
@@ -5,7 +5,7 @@ import android.os.Parcelable
 
 /* Created by Tatjana Kukic and Mert Sevindik on 09.11.2019 */
 
-class TaskList(val name: String?, val tasks: ArrayList<String>? = ArrayList<String>()) : Parcelable {
+class TaskList(val name: String, val tasks: ArrayList<String> = ArrayList<String>()) : Parcelable {
 
     constructor(source: Parcel) : this (source.readString(), source.createStringArrayList())
 
